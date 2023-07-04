346e5cf406526240ee7202f66fc383bcb9e98d35
Roald Ong <roald.ong@gmail.com>
2023-07-05 02:54:30 +0800

    First working version

--- src/laser_filter.cpp
+++ src/laser_filter.cpp
@@ -25,75 +25,74 @@ public:
         RCLCPP_INFO(this->get_logger(), "laser filter started!\n");
         auto laser_callback = [this](const sensor_msgs::msg::LaserScan::SharedPtr msg)
         {
-            // laserscan_message.header.seq = msg->header.seq;
-            // laserscan_message.header.frame_id = msg->header.frame_id;
+            laserscan_message.header.frame_id = msg->header.frame_id;
 
-            // laserscan_message.angle_min = msg->angle_min;
-            // laserscan_message.angle_max = msg->angle_max;
-            std::cout << "Tolerance" << static_cast<double>(msg->ranges.size() - 1) * static_cast<double>(msg->time_increment) << "\n";
-            std::cout << "Array Size" << static_cast<double>(msg->ranges.size() - 1) << "\n";
+            laserscan_message.angle_min = msg->angle_min;
+            laserscan_message.angle_max = msg->angle_max;
+            // std::cout << "Tolerance" << static_cast<double>(msg->ranges.size() - 1) * static_cast<double>(msg->time_increment) << "\n";
+            // std::cout << "Array Size" << static_cast<double>(msg->ranges.size() - 1) << "\n";
 
-            // laserscan_message.angle_increment = msg->angle_increment;
-            // laserscan_message.time_increment = msg->time_increment;
-            // laserscan_message.scan_time = msg->scan_time;
+            laserscan_message.angle_increment = msg->angle_increment;
+            laserscan_message.time_increment = msg->time_increment;
+            laserscan_message.scan_time = msg->scan_time;
 
-            // laserscan_message.time_increment = 1e5;
-            // laserscan_message.scan_time = 0.05;
+            laserscan_message.time_increment = 0.0;
+            laserscan_message.scan_time = 0.0;
 
-            // laserscan_message.range_min = msg->range_min;
-            // laserscan_message.range_max = msg->range_max;
+            laserscan_message.range_min = msg->range_min;
+            laserscan_message.range_max = msg->range_max;
 
-            // laserscan_message.ranges = msg->ranges;
-            // laserscan_message.intensities = msg->intensities;
+            laserscan_message.ranges = msg->ranges;
+            laserscan_message.intensities = msg->intensities;
 
             // std::cout << "received time" << msg->header.stamp.nanosec << "\n";
             // std::cout << "current time" << laserscan_message.header.stamp.nanosec << "\n";
 
-            rclcpp::Time now = this->now();
-            try
-            {
-                tf_buffer_->lookupTransform(
-                    toFrame, fromFrame, tf2::TimePointZero);
-            }
-            catch (const tf2::TransformException &ex)
-            {
-                RCLCPP_INFO(
-                    this->get_logger(), "Could not transform %s to %s: %s",
-                    toFrame.c_str(), fromFrame.c_str(), ex.what());
-                return;
-            }
+            // rclcpp::Time now = this->now();
+            // try
+            // {
+            //     tf_buffer_->lookupTransform(
+            //         toFrame, fromFrame, tf2::TimePointZero);
+            // }
+            // catch (const tf2::TransformException &ex)
+            // {
+            //     RCLCPP_INFO(
+            //         this->get_logger(), "Could not transform %s to %s: %s",
+            //         toFrame.c_str(), fromFrame.c_str(), ex.what());
+            //     return;
+            // }
 
             // // laserscan_message.header.stamp = now;
-            // laserscan_message.header.stamp = rclcpp::Clock().now();
+            laserscan_message.header.stamp = rclcpp::Clock().now();
 
-            // publisher_->publish(laserscan_message);
+            publisher_->publish(laserscan_message);
         };
 
-        timer_ = this->create_wall_timer(
-            50ms, [this]()
-            {
-                // rclcpp::Time current_time = rclcpp::Clock().now();
-                rclcpp::Time current_time = this->get_clock()->now();
+        // timer_ = this->create_wall_timer(
+        //     50ms, [this]()
+        //     {
+        //         // rclcpp::Time current_time = rclcpp::Clock().now();
+        //         rclcpp::Time current_time = this->get_clock()->now();
 
-                laserscan_message.header.frame_id = "base_laser";
-                laserscan_message.angle_min = -2.356194496154785;
-                laserscan_message.angle_max = 2.3557233810424805;
+        //         laserscan_message.header.frame_id = "base_laser";
+        //         laserscan_message.angle_min = -2.356194496154785;
+        //         laserscan_message.angle_max = 2.3557233810424805;
 
-                laserscan_message.angle_increment = 0.00581718236207962;
-                // laserscan_message.time_increment = 6.172839493956417e-05;
-                laserscan_message.time_increment = 0.0;
+        //         laserscan_message.angle_increment = 0.00581718236207962;
+        //         // laserscan_message.time_increment = 6.172839493956417e-05;
+        //         laserscan_message.time_increment = 0.0;
 
-                laserscan_message.ranges = data;
-                laserscan_message.intensities = data;
-                //  laserscan_message.scan_time = 0.05;
-                 laserscan_message.scan_time = 0.0;
+        //         laserscan_message.ranges = data;
+        //         laserscan_message.intensities = data;
+        //         //  laserscan_message.scan_time = 0.05;
+        //          laserscan_message.scan_time = 0.0;
 
-                laserscan_message.range_min = 0.0;
-                laserscan_message.range_max = 100.0;
+        //         laserscan_message.range_min = 0.0;
+        //         laserscan_message.range_max = 100.0;
 
-                laserscan_message.header.stamp = rclcpp::Clock().now();
+        //         laserscan_message.header.stamp = rclcpp::Clock().now();
 
-                publisher_->publish(laserscan_message); });
+        //         publisher_->publish(laserscan_message); });
 
         publisher_ = this->create_publisher<sensor_msgs::msg::LaserScan>("scan", 10);
         subscription_ = this->create_subscription<sensor_msgs::msg::LaserScan>("scan_unfiltered", best_effort.reliability(be), laser_callback);
@@ -116,7 +115,7 @@ private:
     std::shared_ptr<tf2_ros::TransformListener> tf_listener_{nullptr};
     std::string toFrame = "odom";
     std::string fromFrame = "base_laser";
-    rclcpp::TimerBase::SharedPtr timer_;
+    // rclcpp::TimerBase::SharedPtr timer_;
     // const float laser_min = 0.022;
     // const float laser_max = 20;
 };
--- src/odom_convert.cpp
+++ src/odom_convert.cpp
@@ -11,75 +11,99 @@
 
 #define be RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT
 #define PI 3.141592653589793238
+using namespace std::chrono_literals;
 
 class odom_convert : public rclcpp::Node
 {
 public:
-
   odom_convert()
-  : Node("odom_convert"),
-    best_effort(rclcpp::KeepLast(10))
+      : Node("odom_convert"),
+        best_effort(rclcpp::KeepLast(10))
   {
-           tbr = std::make_shared<tf2_ros::TransformBroadcaster>(this);
-           RCLCPP_INFO(this->get_logger(), "odom_converter started!\n");
+    tbr = std::make_shared<tf2_ros::TransformBroadcaster>(this);
+    RCLCPP_INFO(this->get_logger(), "odom_converter started!\n");
+    // rotations = vector<double>(4, 0);
+    // translations = vector<double>(4, 0);
 
+    // define empty initials
+    odom_message = nav_msgs::msg::Odometry();
 
-    auto odom_callback= [this](const labview_r2interface::msg::Lvodom::SharedPtr msg){
-           // std::cout<<msg->bot_x<<" "<<msg->bot_y<<" "<<msg->bot_theta<<" "<<msg->bot_linear<<" "<<msg->bot_angular<<"\n";
-           auto odom_message=nav_msgs::msg::Odometry();
-           odom_message.pose.pose.position.x=msg->bot_x/1000.0;
-           odom_message.pose.pose.position.y=msg->bot_y/1000.0;
-           odom_message.pose.pose.position.z=0;
+    auto odom_callback = [this](const labview_r2interface::msg::Lvodom::SharedPtr msg)
+    {
+      // std::cout<<msg->bot_x<<" "<<msg->bot_y<<" "<<msg->bot_theta<<" "<<msg->bot_linear<<" "<<msg->bot_angular<<"\n";
+      odom_message = nav_msgs::msg::Odometry();
+      odom_message.pose.pose.position.x = msg->bot_x / 1000.0;
+      odom_message.pose.pose.position.y = msg->bot_y / 1000.0;
+      odom_message.pose.pose.position.z = 0;
 
-          //convert from theta in radians to a quaternion
-          tf2::Quaternion q;
-          q.setRPY(0,0,msg->bot_theta);
-          odom_message.pose.pose.orientation.x=q.x();
-          odom_message.pose.pose.orientation.y=q.y();
-          odom_message.pose.pose.orientation.z=q.z();
-          odom_message.pose.pose.orientation.w=q.w();
+      // convert from theta in radians to a quaternion
+      tf2::Quaternion q;
+      q.setRPY(0, 0, msg->bot_theta);
+      odom_message.pose.pose.orientation.x = q.x();
+      odom_message.pose.pose.orientation.y = q.y();
+      odom_message.pose.pose.orientation.z = q.z();
+      odom_message.pose.pose.orientation.w = q.w();
 
-          odom_message.twist.twist.linear.x=msg->bot_linear/1000.0;
-          odom_message.twist.twist.angular.z=msg->bot_angular*(PI/180.0);
-          odom_message.child_frame_id="base_footprint";
-         // get current time and fill up the header
-           rclcpp::Time time_now = rclcpp::Clock().now();
-           odom_message.header.stamp=time_now;
-           odom_message.header.frame_id="odom";
+      odom_message.twist.twist.linear.x = msg->bot_linear / 1000.0;
+      odom_message.twist.twist.angular.z = msg->bot_angular * (PI / 180.0);
+      odom_message.child_frame_id = "base_footprint";
+      // get current time and fill up the header
+      rclcpp::Time time_now = rclcpp::Clock().now();
+      // odom_message.header.stamp = time_now;
+      odom_message.header.stamp = this->now();
+      odom_message.header.frame_id = "odom";
 
-           publisher_->publish(odom_message);
+      // also send transform data at the same rate
+      // traslations[0] = msg->bot_x / 1000.0;
+      // translations[1] = msg->bot_y / 1000.0;
+      // translations[2] = 0.0;
 
-           // also send transform data at the same rate
-           geometry_msgs::msg::TransformStamped transformStamped;
-           transformStamped.header.stamp = time_now;
-            transformStamped.header.frame_id = "odom";
-            transformStamped.child_frame_id = "base_footprint";
-            transformStamped.transform.translation.x = msg->bot_x/1000.0;
-            transformStamped.transform.translation.y = msg->bot_y/1000.0;
-            transformStamped.transform.translation.z = 0.0;
-            transformStamped.transform.rotation.x = q.x();
-            transformStamped.transform.rotation.y = q.y();
-            transformStamped.transform.rotation.z = q.z();
-            transformStamped.transform.rotation.w = q.w();
+      // rotations[0] = q.x();
+      // rotations[1] = q.y();
+      // rotations[2] = q.z();
+      // rotations[3] = q.w();
 
-            tbr->sendTransform(transformStamped);
+      transformStamped.transform.translation.x = msg->bot_x / 1000.0;
+      transformStamped.transform.translation.y = msg->bot_y / 1000.0;
+      transformStamped.transform.translation.z = 0.0;
+      transformStamped.transform.rotation.x = q.x();
+      transformStamped.transform.rotation.y = q.y();
+      transformStamped.transform.rotation.z = q.z();
+      transformStamped.transform.rotation.w = q.w();
     };
 
     publisher_ = this->create_publisher<nav_msgs::msg::Odometry>("odom", 10);
-    subscription_ = this->create_subscription<labview_r2interface::msg::Lvodom>("bot_odom", best_effort.reliability(be),odom_callback);
+    subscription_ = this->create_subscription<labview_r2interface::msg::Lvodom>("bot_odom", best_effort.reliability(be), odom_callback);
 
+    timer_ = this->create_wall_timer(
+        1ms, [this]()
+        {
+
+      // rclcpp::Time current_time = rclcpp::Clock().now();
+      rclcpp::Time current_time = this->get_clock()->now();
+
+      transformStamped.header.stamp = current_time;
+      transformStamped.header.frame_id = "odom";
+      transformStamped.child_frame_id = "base_footprint";
+
+          publisher_->publish(odom_message);
+          tbr->sendTransform(transformStamped); });
   }
 
 private:
+  nav_msgs::msg::Odometry odom_message;
+  geometry_msgs::msg::TransformStamped transformStamped;
+  // vector<double> rotations;
+  // vector<double> translations;
 
   rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr publisher_;
   rclcpp::Subscription<labview_r2interface::msg::Lvodom>::SharedPtr subscription_;
   rclcpp::QoS best_effort;
   std::shared_ptr<tf2_ros::TransformBroadcaster> tbr;
-
+  rclcpp::TimerBase::SharedPtr timer_;
 };
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<odom_convert>());
