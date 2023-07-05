45775dfd4f0052719b8f1b685f9c7d2d403e01ad
Roald Ong <roald.ong@gmail.com>
2023-07-05 14:33:39 +0800

    Updated the lidar_odom launch file

--- launch/lidar_odom.launch.py
+++ launch/lidar_odom.launch.py
@@ -10,26 +10,23 @@ from launch_ros.actions import Node
 def generate_launch_description():
     # this function gets the absolute shared path of the package
     sick_scan = get_package_share_directory("sick_scan2")
-    laser_filters = get_package_share_directory("laser_filters")
 
-    # lidar_launch = IncludeLaunchDescription(
-    #     PythonLaunchDescriptionSource(
-    #         os.path.join(sick_scan, "launch", "sick_tim_5xx.launch.py")
-    #     )
-    # )
-
-    # filter_launch = IncludeLaunchDescription(
-    #     PythonLaunchDescriptionSource(
-    #         os.path.join(laser_filters, "examples", "speckle_filter_example.launch.py")
-    #     )
-    # )
+    lidar_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(sick_scan, "launch", "sick_tim_5xx.launch.py")
+        )
+    )
 
     odom_converter = Node(
         package="labview_r2interface", executable="odom_convert", name="odom_convert"
     )
 
+    laser_converter = Node(
+        package="labview_r2interface", executable="laser_filter", name="laser_filter"
+    )
+
     ld = LaunchDescription()
     ld.add_action(odom_converter)
-    # ld.add_action(lidar_launch)
-    # ld.add_action(filter_launch)
+    ld.add_action(lidar_launch)
+    ld.add_action(laser_converter)
     return ld
