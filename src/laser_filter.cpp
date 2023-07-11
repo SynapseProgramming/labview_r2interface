821c725c5e8ee0823e882decad0b85dc1ff1b196
Roald Ong <roald.ong@gmail.com>
2023-07-11 18:21:29 +0800

    Added in laser filter

--- src/laser_filter.cpp
+++ src/laser_filter.cpp
@@ -19,12 +19,48 @@ public:
         auto laser_callback = [this](const sensor_msgs::msg::LaserScan::SharedPtr msg)
         {
             sensor_msgs::msg::LaserScan unfiltered_laserscan_message = *msg;
+            sensor_msgs::msg::LaserScan filtered_laserscan_message = *msg;
 
+            // unfiltered message properties
             unfiltered_laserscan_message.header.stamp = rclcpp::Clock().now();
             unfiltered_laserscan_message.time_increment = 0.0;
             unfiltered_laserscan_message.scan_time = 0.0;
 
+            // filtered scan properties
+
+            filtered_laserscan_message.header.stamp = rclcpp::Clock().now();
+            filtered_laserscan_message.time_increment = 0.0;
+            filtered_laserscan_message.scan_time = 0.0;
+            std::vector<bool> valid_ranges(filtered_laserscan_message.ranges.size(), false);
+            for (size_t idx = 0; idx < filtered_laserscan_message.ranges.size() - filter_window + 1; ++idx)
+            {
+                bool window_valid = checkWindowValid(
+                    filtered_laserscan_message, idx, filter_window, max_range_difference);
+
+                // Actually set the valid ranges (do not set to false if it was already valid or out of range)
+                for (size_t neighbor_idx_or_self_nr = 0; neighbor_idx_or_self_nr < filter_window; ++neighbor_idx_or_self_nr)
+                {
+                    size_t neighbor_idx_or_self = idx + neighbor_idx_or_self_nr;
+                    if (neighbor_idx_or_self < filtered_laserscan_message.ranges.size()) // Out of bound check
+                    {
+                        bool out_of_range = filtered_laserscan_message.ranges[neighbor_idx_or_self] > max_range;
+                        valid_ranges[neighbor_idx_or_self] = valid_ranges[neighbor_idx_or_self] || window_valid || out_of_range;
+                    }
+                }
+            }
+
+            // set invalid points to nan
+
+            for (size_t idx = 0; idx < valid_ranges.size(); ++idx)
+            {
+                if (!valid_ranges[idx])
+                {
+                    filtered_laserscan_message.ranges[idx] = std::numeric_limits<float>::quiet_NaN();
+                }
+            }
+
             unfilteredpub_->publish(unfiltered_laserscan_message);
+            filteredpub_->publish(filtered_laserscan_message);
         };
 
         // TODO: change the topic names
@@ -33,7 +69,8 @@ public:
         subscription_ = this->create_subscription<sensor_msgs::msg::LaserScan>("scan_unfiltered", best_effort.reliability(be), laser_callback);
     }
 
-    bool checkWindowValid(const sensor_msgs::msg::LaserScan &scan, size_t idx, size_t window, double max_range_difference)
+    bool
+    checkWindowValid(const sensor_msgs::msg::LaserScan &scan, size_t idx, size_t window, double max_range_difference)
     {
         const float &range = scan.ranges[idx];
         if (range != range)
@@ -60,6 +97,18 @@ private:
     rclcpp::Publisher<sensor_msgs::msg::LaserScan>::SharedPtr unfilteredpub_;
     rclcpp::Publisher<sensor_msgs::msg::LaserScan>::SharedPtr filteredpub_;
     rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr subscription_;
+
+
+
+    // TODO: parameterize these values
+
+
+    // only datapoints with distances smaller than this range are taken into account (m)
+    double max_range = 2.0;
+    // maximum distance between two consecutive points (m)
+    double max_range_difference = 0.04;
+    // maximum number of neighbour points to consider
+    unsigned int filter_window = 2;
 
     rclcpp::QoS best_effort;
 };
