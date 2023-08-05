9e79c32e635178f256730df5885453804859955e
Roald Ong <roald.ong@gmail.com>
2023-08-05 13:15:46 +0800

    Added in moving check based off bots velocity

--- src/pose_reset.cpp
+++ src/pose_reset.cpp
@@ -18,7 +18,7 @@ class pose_reset : public rclcpp::Node
 {
 public:
     pose_reset()
-        : Node("pose_reset_new"),
+        : Node("pose_reset"),
           best_effort(rclcpp::KeepLast(10))
     {
         RCLCPP_INFO(this->get_logger(), "pose_reset started!\n");
@@ -39,21 +39,20 @@ public:
         cov[28] = 1e-9;
         // rz variance
         cov[35] = 1e-9;
-        auto cmdvel_callback = [this](const geometry_msgs::msg::Twist::SharedPtr msg)
-        {
-            if (msg->linear.x == 0 && msg->angular.z == 0)
-                ismoving = false;
-            else
-                ismoving = true;
-        };
 
         auto odom_callback = [this](const nav_msgs::msg::Odometry::SharedPtr msg)
         {
-            this->initOdom = *msg;
+            double linearv = msg->twist.twist.linear.x;
+            double angularv = msg->twist.twist.angular.z;
+            if (abs(angularv) <= 0.01 && abs(linearv) < 0.01)
+                ismoving = false;
+            else
+                ismoving = true;
+
+            std::cout << ismoving << "\n";
         };
 
         publisher_ = this->create_publisher<geometry_msgs::msg::PoseWithCovarianceStamped>("/set_pose", 10);
-        subscription_ = this->create_subscription<geometry_msgs::msg::Twist>("cmd_vel", best_effort.reliability(be), cmdvel_callback);
         odomsub_ = this->create_subscription<nav_msgs::msg::Odometry>("odom", best_effort.reliability(be), odom_callback);
 
         timer_ = this->create_wall_timer(
@@ -67,22 +66,19 @@ public:
                 else if (fired == false)
                 {
                     this->one_off_timer->reset();
-                    //   only call timer when there is odom data
-                    if(initOdom.pose.pose.position.x==0) return;
-                    latchedOdom = initOdom;
                     fired = true;
                 } });
 
         one_off_timer = this->create_wall_timer(600s, [this]()
                                                 {
-                    geometry_msgs::msg::PoseWithCovarianceStamped amci;
-                    amci.header.frame_id= "odom";
-                    amci.header.stamp = this->get_clock()->now();
-                    amci.pose = latchedOdom.pose;
+                                                    // geometry_msgs::msg::PoseWithCovarianceStamped amci;
+                                                    // amci.header.frame_id= "odom";
+                                                    // amci.header.stamp = this->get_clock()->now();
+                                                    // amci.pose = latchedOdom.pose;
 
-                    publisher_->publish(amci);
+                                                    // publisher_->publish(amci);
 
-                    this->one_off_timer->reset(); });
+                                                    this->one_off_timer->reset(); });
         // cancel to prevent running at the start
         one_off_timer->cancel();
     };
@@ -98,7 +94,6 @@ private:
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::TimerBase::SharedPtr one_off_timer;
 
-    nav_msgs::msg::Odometry initOdom;
     nav_msgs::msg::Odometry latchedOdom;
 
     std::array<double, 36> cov;
